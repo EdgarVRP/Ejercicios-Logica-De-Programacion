8c3635a85d0cc25a6a28b6c5e13aab1022513079
EdgarVRP <edgarvrp22@gmail.com>
2023-02-28 09:54:02 -0600

    Ejercicio 2 completo

--- Ejercicio de Lógica de Programación 2/Ejercicio-2.js
+++ Ejercicio de Lógica de Programación 2/Ejercicio-2.js
@@ -16,12 +16,17 @@ while (isNaN(celcius)) {
         }
     }   
 }
-
+//asignando div de html
+let resultado=document.getElementById("resultado");
 //Mostrando grado fahrenheit
 console.log("Grado Fahrenheit: "+celciusFahrenheit(celcius));
 localStorage.setItem("GradosFahrenheit",JSON.stringify(celciusFahrenheit(celcius)));
+//Mostrando grado fahrenheit en html
+resultado.innerHTML="Grado Fahrenheit: "+celciusFahrenheit(celcius)+"<br>";
 //Mostrando grado kelvin
 console.log("Grado Kelvin: "+celciusKelvin(celcius));
 localStorage.setItem("GradosKelvin",JSON.stringify(celciusKelvin(celcius)));
 //Mostrando con alert
+//Mostrando grado kelvin en html
+resultado.innerHTML+="Grado Kelvin: "+celciusKelvin(celcius)+"<br>";
 alert("Grado Fahrenheit: "+celciusFahrenheit(celcius)+"\nGrado Kelvin: "+celciusKelvin(celcius));
